145e72611c24cafb8303e44d617d78d6a9ea5b01
rsdesouza <sdesouza.rodolfo@gmail.com>
2023-11-18 13:39:56 -0300

    Ajuste

--- variables.tf
+++ variables.tf
@@ -26,10 +26,10 @@ variable "frontend_image" {
   }
 }
 
-variable "db_password" {
-  description = "Senha para o banco de dados"
-  validation {
-    condition     = can(regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", var.db_password))
-    error_message = "A senha do banco de dados deve conter pelo menos 8 caracteres, incluindo pelo menos uma letra e um número."
-  }
-}
+#variable "db_password" {
+ # description = "Senha para o banco de dados"
+  #validation {
+   # condition     = can(regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", var.db_password))
+    #error_message = "A senha do banco de dados deve conter pelo menos 8 caracteres, incluindo pelo menos uma letra e um número."
+ # }
+#}
